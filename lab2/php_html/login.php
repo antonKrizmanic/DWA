c1f325478c7f1859e8a7e2e89320aa059c7f77ae
cupko55 <akrizmani@tvz.hr>
2016-03-21 13:42:17 +0100

    Verzija 1.0

--- lab2/php_html/login.php
+++ lab2/php_html/login.php
@@ -10,7 +10,7 @@
 		<div class="container">
 			<header class="page-header">
 				<div class="row">
-					<div class="col-sm-6">
+					<div class="col-sm-6">						
 						<img src="../img/fantBeastsLogo.png" alt="Fantastic beasts logo" class="img-responsive">
 					</div>
 					<div class="col-sm-2 col-sm-offset-4">
@@ -30,25 +30,25 @@
 					</ul>
 				</nav>
 				<section class="col-sm-10 content">
-					<article class="col-sm-6">
+					<article class="col-sm-6">						
 						<h2>Osobni podaci:</h2>
 						<p>Ime i prezime: Anton Krizmanić</p>
 						<p>Datum rođenja:08.10.1994</p>
 						<p>Mjesto rođenja:Rijeka</p>
 					</article>
-					<article class="col-sm-6">
+					<article class="col-sm-6">						
 						<h2>Podaci o školovanju:</h2>
 						<p>Osnovna škole: Osnovna škola Marije i Line Umag, Umag</p>
 						<p>Srednja škola: Gospodarska škola Buje, Buje</p>
 						<p>Fakultet: Tehničko veleučilište Zagreb, Zagreb</p>
 					</article>
-					<article class="col-sm-6">
+					<article class="col-sm-6">						
 						<h2>Podaci o radnom iskustvu</h2>
 						<p>Prometna jedinica mladeži, Umag 2010 godine</p>
 						<p>Vozač eko vozila u Istraturistu 3 sezone</p>
 
 					</article>
-					<article class="col-sm-6">
+					<article class="col-sm-6">						
 						<h2>Znanja i vještine</h2>
 						<p>C,C++,C#,HTML,CSS,Javascript,PHP,ASP.NET</p>
 					</article>
