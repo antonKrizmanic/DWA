d52070e0092c23835725fbd2bf94a5beddfb8319
cupko55 <akrizmani@tvz.hr>
2016-04-13 15:26:17 +0200

    lab4

---
+++ lab4/php_html/login.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Fantastic beasts</title>
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/stil.css">
+	<?php
+		session_start();
+		$_SESSION["username"]="korisnik";
+		$_SESSION["password"]="korisnik";
+	?>
+</head>
+<body>	
+		<div class="reklama container">
+			<p>Ovo je reklama</p>
+			<p id="zat" onclick="zatvori()">Zatvori reklamu</p>
+		</div>
+		<div class="container">
+			<header class="page-header">
+				<a href="#osobni_podaci">Osobni podaci</a>
+				<a href="#podaci_skolovanje">Podaci o skolovanju</a>
+				<a href="#podaci_o_radu">Radno iskustvo</a>
+				<a href="#vjestine">Vjestine</a>
+				<div class="row">
+					<div class="col-sm-6">						
+						<img src="../img/fantBeastsLogo.png" alt="Fantastic beasts logo" class="img-responsive">
+					</div>
+					<div class="col-sm-2 col-sm-offset-4">
+						<?php								
+							if($_SESSION["username"]==$_GET["username"] && $_SESSION["password"]==$_GET["password"]){
+								echo "Korisnik:".$_GET["username"]."</br>";
+								echo "Lozinka:".$_GET["password"]."";
+							}
+							else{
+								header("Location: https://moj.tvz.hr/index.php?prikaz=nastsem1");
+							}
+						?>						
+						<a href="login.html" class="odjava">Odjava</a>
+					</div>
+				</div>
+			</header>			
+				<nav class="col-sm-2">
+					<ul class="nav">
+						<li><a href="#">Zivotopis</a></li>
+						<li><a href="popisProizvoda.php">Popis proizvoda</a></li>
+						<li><a href="popisProizvodaIdiorm.php">Popis proizvoda Idiorm</a></li>
+						<li><a href="unosForme.php">Unos proizvoda</a></li>
+						<li><a href="#">Opcija 4</a></li>
+					</ul>
+				</nav>
+				<section class="col-sm-10 content">
+					
+					<article class="col-sm-12">						
+						<h2 id="osobni" onclick="sakrij(this.id)">Osobni podaci:</h2>
+						<div id="osobniPodaci">
+							<p>Ime i prezime: Anton Krizmanić</p>
+							<p>Datum rođenja:08.10.1994</p>
+							<p>Mjesto rođenja:Rijeka</p>
+						</div>
+					</article>
+					<article class="col-sm-12">						
+						<h2 id="skolovanje" onclick="sakrij(this.id)">Podaci o školovanju:</h2>
+						<div id="skolovanjePodaci">
+							<p>Osnovna škole: Osnovna škola Marije i Line Umag, Umag</p>
+							<p>Srednja škola: Gospodarska škola Buje, Buje</p>
+							<p>Fakultet: Tehničko veleučilište Zagreb, Zagreb</p>
+						</div>
+
+					</article>
+					<article class="col-sm-12">						
+						<h2 id="rad" onclick="sakrij(this.id)">Podaci o radnom iskustvu</h2>
+						<div id="radPodaci">
+							<p>Prometna jedinica mladeži, Umag 2010 godine</p>
+							<p>Vozač eko vozila u Istraturistu 3 sezone</p>
+						</div>
+
+					</article>
+					<article class="col-sm-12">						
+						<h2 id="vjestine" onclick="sakrij(this.id)">Znanja i vještine</h2>
+						<div id="vjestinePodaci">
+							<p>C,C++,C#,HTML,CSS,Javascript,PHP,ASP.NET</p>
+						</div>
+					</article>			
+				</section>
+			
+		</div>
+		
+		<div class="footer-wrapper">		
+				<footer class="container" >		
+						<p>Copyright Fantastic beasts, 2016</p>		
+				</footer>		
+		</div>
+			<script type="text/javascript">
+			function sakrij(element){
+				var id=element+"Podaci";				
+				var div=document.getElementById(id);
+				if(div.style.display=="block"){
+					div.style.display="none";
+				}
+				else {
+					div.style.display="block";
+				}
+			}
+			function zatvori(){
+				var div=document.querySelector(".reklama");
+				
+				div.style.visibility="hidden";
+			}
+		</script>
+</body>
+</html>
---
+++ lab4/php_html/popisProizvoda.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Fantastic beasts</title>
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/stil.css">
+	<?php		
+		include("connect.php");
+		session_start();
+	?>
+</head>
+<body>	
+		
+		<div class="container">
+			<header class="page-header">				
+				<div class="row">
+					<div class="col-sm-6">						
+						<img src="../img/fantBeastsLogo.png" alt="Fantastic beasts logo" class="img-responsive">
+					</div>
+					<div class="col-sm-2 col-sm-offset-4">
+											
+						<a href="login.html" class="odjava">Odjava</a>
+					</div>
+				</div>
+			</header>			
+				<nav class="col-sm-2">
+					<ul class="nav">
+						<li><a href="login.php">Zivotopis</a></li>
+						<li><a href="#">Popis proizvoda</a></li>
+						<li><a href="popisProizvodaIdiorm.php">Popis proizvoda Idiorm</a></li>
+						<li><a href="unosForme.php">Unos proizvoda</a></li>
+						<li><a href="#">Opcija 4</a></li>
+					</ul>
+				</nav>
+				<section class="col-sm-10 content">
+
+					<form class="form-inline">
+						<div class="form-group">
+							<label for="search">Trazeni pojam:</label>
+							<input type="text" id="search" name="search" placeholder="Upisi trazeni pojam" class="form-control ">							
+						</div>						
+						<input type="submit" id="submit" onclick="trazi()" class="btn btn-default" value="Pretraži stranicu">
+						
+					</form>
+					
+					<table class="table table-striped" id="tablica">
+						<thead>
+							<tr>
+								<th>Naziv proizvoda</th>
+								<th>Vrsta proizvoda</th>
+								<th>Zivotinja</th>
+								<th>Opis</th>
+								<th>Cijena</th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php						
+							$query="SELECT proizvodi.naziv,proizvod.vrstaProizvoda,zivotinje.nazivZivotinje,opisProizvoda,cijena,proizvodi.id as id
+									FROM proizvodi
+									JOIN proizvod ON proizvodi.tipProizvoda=proizvod.id
+									JOIN zivotinje ON proizvodi.tipZivotinje=zivotinje.id
+									ORDER BY id;";
+
+							if($result=mysqli_query($link,$query))
+
+							while($row=mysqli_fetch_row($result)){
+								echo"
+								<tr>
+									<td>$row[0]</td>
+									<td>$row[1]</td>
+									<td>$row[2]</td>
+									<td  style='width:200px'>$row[3]</td>
+									<td>$row[4] kn</td>
+								</tr>
+								";
+							}	
+
+						?>
+						</tbody>
+					</table>					
+				</section>
+			
+		</div>
+		
+		<div class="footer-wrapper">		
+				<footer class="container" >		
+						<p>Copyright Fantastic beasts, 2016</p>		
+				</footer>		
+		</div>	
+		<script type="text/javascript">
+			function trazi(){
+				var unos=document.getElementById("search").value;
+				var trs = document.getElementsByTagName("tr");
+				
+				for(var i=1;i<trs.length;i++){
+					var c=trs[i].innerHTML.search(unos);
+					if(c==-1){
+						trs[i].style.display="none";	
+					}
+				}
+			}
+		</script>
+</body>
+</html>
