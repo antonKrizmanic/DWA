bb0d649d004993bf6b09720521de4fc04530104c
cupko55 <akrizmani@tvz.hr>
2016-06-05 18:37:28 +0200

    lab8

--- lab8/lab8/logic/popisProizvoda.php
+++ lab8/lab8/logic/popisProizvoda.php
@@ -6,16 +6,33 @@ ORM::configure('password', 'root');
 
 // iduća linija je potrebna kako bi natjerali konekciju prema MySQL-u u utf8
 ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-$unos=$_GET['var'];
-	// podaci iz baze
+
+if(isset($_GET['function'])) {
+    if($_GET['function'] == '1') {
+        getAll();
+    } elseif($_GET['function'] == '2') {
+        getSearch($_GET['var']);
+    }
+}
+function getAll(){
 	$products=ORM::for_table('proizvodi')->select_many(array('Naziv'=>'proizvodi.naziv','Vrsta'=>'proizvod.vrstaProizvoda','Zivotinja'=>'zivotinje.nazivZivotinje',
+								'Opis'=>'proizvodi.opisProizvoda','Cijena','id'=>'proizvodi.id'))->
+							join('proizvod',array('proizvodi.tipProizvoda','=','proizvod.id'))->
+							join('zivotinje',array('proizvodi.tipZivotinje','=','zivotinje.id'))->							
+							order_by_asc('id')->
+							find_array();
+echo json_encode($products);
+}
+function getSearch($unos){
+$products=ORM::for_table('proizvodi')->select_many(array('Naziv'=>'proizvodi.naziv','Vrsta'=>'proizvod.vrstaProizvoda','Zivotinja'=>'zivotinje.nazivZivotinje',
 								'Opis'=>'proizvodi.opisProizvoda','Cijena','id'=>'proizvodi.id'))->
 							join('proizvod',array('proizvodi.tipProizvoda','=','proizvod.id'))->
 							join('zivotinje',array('proizvodi.tipZivotinje','=','zivotinje.id'))->
 							where_like('Naziv', "%$unos%")->							
 							order_by_asc('id')->
-							find_array();
-
-/*where_like('Naziv', "%$unos%")->*/
-echo json_encode($products);
+							find_array();							
+echo json_encode($products);	
+}
+	
+	
 ?>
